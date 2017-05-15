d8deead9df68e8740c4c78929d01eb002ef6ef7d
Zheng Tian <zngtian@gmail.com>
2017-05-15 13:14:05 +0800

    Half in Lecture 3

---
+++ lecturenotes/lecture_3/auto/slide_lecture_3.el
@@ -0,0 +1,61 @@
+(TeX-add-style-hook
+ "slide_lecture_3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "amsthm"
+    "mathtools"
+    "booktabs"
+    "color"
+    "caption"
+    "subcaption")
+   (TeX-add-symbols
+    '("undersetdisp" 2)
+    '("rarrowd" 1)
+    "dx"
+    "var"
+    "cov"
+    "corr"
+    "pr"
+    "plim"
+    "plimn"
+    "mathbi")
+   (LaTeX-add-labels
+    "sec:org432edfa"
+    "sec:org357ca55"
+    "eq:garchms"
+    "sec:org0171ce2"
+    "sec:orgb368a05"
+    "eq:arch-logL")
+   (LaTeX-add-amsthm-newtheorems
+    "mydef"
+    "mythm"))
+ :latex)
+
